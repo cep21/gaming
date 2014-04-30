654ec71f5652399e4394df37746de1d8e7b0ce60
Jack Lindamood <cep221@gmail.com>
2014-04-29 19:27:30 -0700

    make file compile

--- src/sim/sim.go
+++ src/sim/sim.go
@@ -6,26 +6,26 @@
 package main
 
 import (
-	"gaming"
-	"blackjack"
+//	"gaming"
+//	"blackjack"
 	"flag"
-	"os"
-	"math/rand"
+//	"os"
+//	"math/rand"
 	"time"
 )
 
 func main() {
-	oddsA := flag.Float64("odda", .5, "Odds of option A")
-	oddsB := flag.Float64("oddb", .5, "Odds of option B")
+//	oddsA := flag.Float64("odda", .5, "Odds of option A")
+//	oddsB := flag.Float64("oddb", .5, "Odds of option B")
 	seed := flag.Int("seed", 0, "Random seed for simulation")
 	flag.Parse()
 	if *seed == 0 {
 		*seed = int(time.Now().Unix())
 	}
-	r := rand.New(rand.NewSource(int64(*seed)))
+//	r := rand.New(rand.NewSource(int64(*seed)))
 	for i:=0;i<10000;i++ {
-		v1 := r.Float64() * oddsA
-		v2 := r.Float64() * oddsA
+//		v1 := r.Float64() * oddsA
+//		v2 := r.Float64() * oddsA
 	}
 }
 
